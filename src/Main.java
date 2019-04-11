4e90a1f305eaa6a6a0d565d9945d704604220988
Nacho Gallego <isalceda.mis2016@student.ie.edu>
2019-04-11 12:08:11 +0200

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,28 @@ public class Main {
 
     public static void main(String[] args) {
         String emailAddress = "john.doe.mis2016@ie.edu";
-        String formattedEmailAddress = emailAddress.replaceAll(".", "/");
+        String formattedEmailAddress = emailAddress.replaceAll("\\.", "/");
 
         Random random = new Random();
-        StringBuffer word = null;
+        StringBuffer word = new StringBuffer();
+        word.delete(0, word.length());
+        String firstLetter = new String();
+
+
 
         switch (random.nextInt(2)) {
             case 0:
-                word = new StringBuffer('Y');
+                firstLetter = "Y";
+                break;
             case 1:
-                word = new StringBuffer('F');
+                firstLetter = "F";
+                break;
             case 2:
-                word = new StringBuffer('T');
+                firstLetter = "T";
+                break;
         }
 
+        word.append(firstLetter);
         word.append('o');
         word.append('u');
         word.append('r');
